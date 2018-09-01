36f604ac1e5a81f60501478a0826403ba3edccae
James Dearlove <jamesdearlove@outlook.com>
2018-09-01 15:37:11 +1000

    Added meme detection and communication with server

--- base64.js
+++ base64.js
@@ -1,28 +1,40 @@
-var images  = document.getElementsByTagName("img");
+var images = document.getElementsByTagName("img");
 var imageSrc = images[2].src;
 var imgSrcArray = [];
 var base64Array = [];
+var isImgMeme = [];
 
-function convertImgToBase64(url, callback, outputFormat){
+function convertImgToBase64(url, callback, outputFormat) {
     var img = new Image();
     img.crossOrigin = 'Anonymous';
-    img.onload = function(){
+    img.onload = function () {
         var canvas = document.createElement('CANVAS');
         var ctx = canvas.getContext('2d');
         canvas.height = this.height;
         canvas.width = this.width;
-        ctx.drawImage(this,0,0);
+        ctx.drawImage(this, 0, 0);
         var dataURL = canvas.toDataURL(outputFormat || 'image/png').replace(/^data:image\/(png|jpg);base64,/, '');
         callback(dataURL);
-        canvas = null; 
+        canvas = null;
     };
     img.src = url;
 }
 
-for (i = 0; i< images.length; i++) {
+function determineMeme(image) {
+    var xhr = new XMLHttpRequest();
+    xhr.open('POST', 'https://us-central1-deletthis-123.cloudfunctions.net/cloud_is_meme', true);
+    xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+    xhr.onload = function () {
+        console.log(this.responseText); 
+    };
+    xhr.send(encodeURIComponent('image') + '=' + encodeURIComponent(image));
+}
+
+for (i = 0; i < images.length; i++) {
     if ((images[i].width > 100) && (images[i].height > 100)) {
-        convertImgToBase64(images[i].src, function(base64Img){
+        convertImgToBase64(images[i].src, function (base64Img) {
             base64Array.push(base64Img);
+            determineMeme(base64Img);
         })
     }
 }
