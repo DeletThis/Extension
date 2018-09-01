c1257d192e9ec73c73bbb06c55d568d9575d5fe4
James Dearlove <jamesdearlove@outlook.com>
2018-09-01 18:50:41 +1000

    MVP - Added removal of memes and replaces with image

--- background.js
+++ background.js
@@ -5,7 +5,6 @@ chrome.runtime.onInstalled.addListener(function() {
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
         chrome.declarativeContent.onPageChanged.addRules([{
           conditions: [new chrome.declarativeContent.PageStateMatcher({
-            pageUrl: {hostEquals: 'imgur.com'},
           })
           ],
               actions: [new chrome.declarativeContent.ShowPageAction()]
