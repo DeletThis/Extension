c64763d05f7544437e3c49d74a85f6f21cad65dd
Ishraque Zahin <30386992+Ishraque99@users.noreply.github.com>
2018-09-01 13:05:14 +1000

    Initial Commit
    all the base files for an unpacked chrome extention

---
+++ base64.js
@@ -0,0 +1,22 @@
+var images  = document.getElementsByTagName("img");
+var imageSrc = images[2].src;
+
+function convertImgToBase64(url, callback, outputFormat){
+    var img = new Image();
+    img.crossOrigin = 'Anonymous';
+    img.onload = function(){
+        var canvas = document.createElement('CANVAS');
+        var ctx = canvas.getContext('2d');
+        canvas.height = this.height;
+        canvas.width = this.width;
+        ctx.drawImage(this,0,0);
+        var dataURL = canvas.toDataURL(outputFormat || 'image/png');
+        callback(dataURL);
+        canvas = null; 
+    };
+    img.src = url;
+}
+
+convertImgToBase64(imageSrc, function(base64Img){
+    console.log('IMAGE:',base64Img);
+})
